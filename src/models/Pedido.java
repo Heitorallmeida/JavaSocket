6ce145205f03305cd51f57ea75e98eb8a78e5c66
aldemirfilho <amrf@ic.ufal.br>
2021-09-07 10:50:42 -0300

    Acrescimo de funcoes na classe Pedido

--- src/models/Pedido.java
+++ src/models/Pedido.java
@@ -1,20 +1,38 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
 public class Pedido {
-    HashMap<Produto, Integer> produtos;
-    Double preco;
-    Date data;
+    //HashMap<Produto, Integer> produtos; não seria melhor uma lista de produtos pra compor o pedido?
+    private ArrayList<Produto> produtos;
+    private Double preco;
+    private Date data;
 
-    public HashMap<Produto, Integer> getProdutos() {
-        return produtos;
+    public void add_produto(Produto produto){
+        this.produtos.add(produto);
     }
 
-    public void setProdutos(HashMap<Produto, Integer> produtos) {
-        this.produtos = produtos;
+    public Double total_pedido(){
+        for(int i = 0; i < this.produtos.size(); i++){
+            this.preco = this.produtos.get(i).getPreco() + this.preco;
+        }
+        return this.preco;
     }
+
+    public void remove_produto(Produto produto){
+        for(int i = 0; i < this.produtos.size(); i++){
+            if(this.produtos.get(i).getNome().equals(produto.getNome()){
+                this.preco = this.preco - produto.getPreco();
+                this.produtos.remove(i);
+            }
+        }
+    }
+
+    public ArrayList<Produto> getProdutos() { return produtos; }
+
+    public void setProdutos(ArrayList<Produto> produtos) { this.produtos = produtos; }
 
     public Double getPreço() {
         return preco;
