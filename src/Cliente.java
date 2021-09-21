268dc150a5bf7eb063f4897ebe717d664374cfd1
Heitor <heitor.almeida@accio.net.br>
2021-09-21 00:03:39 -0300

    ajuste

--- src/Cliente.java
+++ src/Cliente.java
@@ -24,9 +24,12 @@ class Cliente {
         System.out.println("-----------------------------------------------------------");
         System.out.println("-----------------------------------------------------------");
 
+        byte[] b = InetAddress.getByName(ip).getAddress();
+
+
         int meuId = -1;
         while(!sentence.equals("sair")) {
-            Socket clientSocket = new Socket(ip, 5566);
+            Socket clientSocket = new Socket(InetAddress.getByAddress(b).getHostName(), 5566);
 
             ArrayList<String> titleList;
 
